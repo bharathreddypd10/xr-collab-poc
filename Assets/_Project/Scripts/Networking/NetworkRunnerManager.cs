606477b74a892c95ecda6f40945130b15022cd40
Bharath <bharath.patlolla@yottatechports.com>
2026-05-21 18:34:59 +0530

    Set up Fusion networking bootstrap and test scene

---
+++ Assets/_Project/Scripts/Networking/NetworkRunnerManager.cs
@@ -0,0 +1,96 @@
+using Fusion;
+using Fusion.Sockets;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class NetworkRunnerManager : MonoBehaviour, INetworkRunnerCallbacks
+{
+    public NetworkPrefabRef playerPrefab;
+
+    private NetworkRunner runner;
+
+    async void Start()
+    {
+        runner = gameObject.AddComponent<NetworkRunner>();
+
+        runner.ProvideInput = true;
+
+        var sceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>();
+
+        runner.AddCallbacks(this);
+
+        await runner.StartGame(new StartGameArgs()
+        {
+            GameMode = GameMode.Shared,
+            SessionName = "TestRoom",
+            SceneManager = sceneManager,
+            PlayerCount = 4
+        });
+    }
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        if (player == runner.LocalPlayer)
+        {
+            Vector3 spawnPosition = new Vector3(0, 1, 0);
+
+            runner.Spawn(
+                playerPrefab,
+                spawnPosition,
+                Quaternion.identity,
+                player
+            );
+        }
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
+
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+        Vector2 move = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            move.y += 1f;
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            move.y -= 1f;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            move.x -= 1f;
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            move.x += 1f;
+
+        input.Set(new NetworkInputData { Move = move.normalized });
+    }
+
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+
+    public void OnConnectedToServer(NetworkRunner runner) { }
+
+    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) { }
+
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data) { }
+
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
+
+    public void OnSceneLoadDone(NetworkRunner runner) { }
+
+    public void OnSceneLoadStart(NetworkRunner runner) { }
+
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+}
