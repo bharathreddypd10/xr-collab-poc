824fb280262c8f9128e8877a30ef2ab86ae0db84
Bharath <bharath.patlolla@yottatechports.com>
2026-05-22 19:37:26 +0530

    feat: add Fusion shared-mode multiplayer setup

--- Assets/_Project/Scripts/Networking/NetworkRunnerManager.cs
+++ Assets/_Project/Scripts/Networking/NetworkRunnerManager.cs
@@ -1,96 +1,227 @@
 using Fusion;
 using Fusion.Sockets;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using System;
 using System.Collections.Generic;
 
 public class NetworkRunnerManager : MonoBehaviour, INetworkRunnerCallbacks
 {
-    public NetworkPrefabRef playerPrefab;
+    // Assign PlayerCapsule prefab in Inspector
+    // Prefab MUST contain:
+    // - NetworkObject
+    // - NetworkTransform
+
+    public NetworkObject playerPrefab;
 
     private NetworkRunner runner;
 
     async void Start()
     {
+        Debug.Log("[Fusion] Creating Runner");
+
         runner = gameObject.AddComponent<NetworkRunner>();
 
         runner.ProvideInput = true;
 
-        var sceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>();
+        var sceneManager =
+            gameObject.AddComponent<NetworkSceneManagerDefault>();
 
         runner.AddCallbacks(this);
+
+        Debug.Log("[Fusion] Starting Game");
 
         await runner.StartGame(new StartGameArgs()
         {
             GameMode = GameMode.Shared,
             SessionName = "TestRoom",
+
+            Scene = SceneRef.FromIndex(
+                SceneManager.GetActiveScene().buildIndex
+            ),
+
             SceneManager = sceneManager,
+
             PlayerCount = 4
         });
+
+        Debug.Log("[Fusion] StartGame Complete");
     }
 
     public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
     {
-        if (player == runner.LocalPlayer)
-        {
-            Vector3 spawnPosition = new Vector3(0, 1, 0);
+        Debug.Log($"[Fusion] Player Joined: {player}");
 
-            runner.Spawn(
+        // In Shared Mode:
+        // each player spawns THEIR OWN object
+        if (runner.LocalPlayer == player)
+        {
+            Vector3 spawnPosition =
+                new Vector3(
+                    player.RawEncoded * 3,
+                    1,
+                    0
+                );
+
+            Debug.Log($"[Fusion] Spawning at {spawnPosition}");
+
+            var spawnedObject = runner.Spawn(
                 playerPrefab,
                 spawnPosition,
                 Quaternion.identity,
                 player
             );
+
+            if (spawnedObject != null)
+            {
+                Debug.Log(
+                    $"[Fusion] Spawn SUCCESS: {spawnedObject.name}"
+                );
+            }
+            else
+            {
+                Debug.LogError("[Fusion] Spawn FAILED");
+            }
         }
     }
 
-    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+        Debug.Log($"[Fusion] Player Left: {player}");
+    }
 
     public void OnInput(NetworkRunner runner, NetworkInput input)
     {
         Vector2 move = Vector2.zero;
 
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.W) ||
+            Input.GetKey(KeyCode.UpArrow))
+        {
             move.y += 1f;
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-            move.y -= 1f;
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-            move.x -= 1f;
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-            move.x += 1f;
+        }
 
-        input.Set(new NetworkInputData { Move = move.normalized });
+        if (Input.GetKey(KeyCode.S) ||
+            Input.GetKey(KeyCode.DownArrow))
+        {
+            move.y -= 1f;
+        }
+
+        if (Input.GetKey(KeyCode.A) ||
+            Input.GetKey(KeyCode.LeftArrow))
+        {
+            move.x -= 1f;
+        }
+
+        if (Input.GetKey(KeyCode.D) ||
+            Input.GetKey(KeyCode.RightArrow))
+        {
+            move.x += 1f;
+        }
+
+        input.Set(new NetworkInputData
+        {
+            Move = move.normalized
+        });
     }
 
-    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+    public void OnInputMissing(
+        NetworkRunner runner,
+        PlayerRef player,
+        NetworkInput input)
+    {
+    }
 
-    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+    public void OnShutdown(
+        NetworkRunner runner,
+        ShutdownReason shutdownReason)
+    {
+    }
 
-    public void OnConnectedToServer(NetworkRunner runner) { }
+    public void OnConnectedToServer(NetworkRunner runner)
+    {
+    }
 
-    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) { }
+    public void OnDisconnectedFromServer(
+        NetworkRunner runner,
+        NetDisconnectReason reason)
+    {
+    }
 
-    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+    public void OnConnectRequest(
+        NetworkRunner runner,
+        NetworkRunnerCallbackArgs.ConnectRequest request,
+        byte[] token)
+    {
+    }
 
-    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+    public void OnConnectFailed(
+        NetworkRunner runner,
+        NetAddress remoteAddress,
+        NetConnectFailedReason reason)
+    {
+    }
 
-    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+    public void OnUserSimulationMessage(
+        NetworkRunner runner,
+        SimulationMessagePtr message)
+    {
+    }
 
-    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+    public void OnSessionListUpdated(
+        NetworkRunner runner,
+        List<SessionInfo> sessionList)
+    {
+    }
 
-    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+    public void OnCustomAuthenticationResponse(
+        NetworkRunner runner,
+        Dictionary<string, object> data)
+    {
+    }
 
-    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+    public void OnHostMigration(
+        NetworkRunner runner,
+        HostMigrationToken hostMigrationToken)
+    {
+    }
 
-    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data) { }
+    public void OnReliableDataReceived(
+        NetworkRunner runner,
+        PlayerRef player,
+        ReliableKey key,
+        ArraySegment<byte> data)
+    {
+    }
 
-    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
+    public void OnReliableDataProgress(
+        NetworkRunner runner,
+        PlayerRef player,
+        ReliableKey key,
+        float progress)
+    {
+    }
 
-    public void OnSceneLoadDone(NetworkRunner runner) { }
+    public void OnSceneLoadDone(NetworkRunner runner)
+    {
+        Debug.Log("[Fusion] Scene Load Done");
+    }
 
-    public void OnSceneLoadStart(NetworkRunner runner) { }
+    public void OnSceneLoadStart(NetworkRunner runner)
+    {
+        Debug.Log("[Fusion] Scene Load Start");
+    }
 
-    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnObjectExitAOI(
+        NetworkRunner runner,
+        NetworkObject obj,
+        PlayerRef player)
+    {
+    }
 
-    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnObjectEnterAOI(
+        NetworkRunner runner,
+        NetworkObject obj,
+        PlayerRef player)
+    {
+    }
 }
